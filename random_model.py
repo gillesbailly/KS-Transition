45d28200d2d08edaf40bbe708e305a7c26460fd4
gillesbailly <gilles.bailly@sorbonne-universite.fr>
2020-02-03 13:40:59 +0100

    Random is fixed for technique as input variable

--- random_model.py
+++ random_model.py
@@ -16,18 +16,29 @@ class Random_Model(Model):
         super().__init__("random", env)
         self.description = "In this model, we assume that participants do not engage with the task at all and simply choose methods at random, perhaps with a bias for one option over the other. Such random behavior is not uncommon in behavioral experiments, especially when participants have no external incentives for performing well"
         #self.params = Parameters('./parameters/random_model.csv')
+        self.reset(self.available_strategies)
 
     ##########################
     def action_probs(self, cmd, date):
         actions = self.get_actions_from( cmd )
+        n = len(actions)
         b = self.params.value['b']
-        prob = [ b ]
-        if len(actions) == 2:   # Menu and Hotkey actions
-            prob.append( 1.-b )
-        else:                   # Menu, Hotkey and Learning actions
-            prob.append( (1.-b) /2.)
-            prob.append( (1.-b) /2.)
-        return prob
+        probs = np.zeros( n )
+
+        if n == 1:
+            return [1.]
+
+        default_strategy = Strategy.MENU
+        if not (default_strategy in self.available_strategies):
+            default_strategy = Strategy.LEARNING
+            if not (default_strategy in self.available_strategies):
+                default_strategy = Strategy.HOTKEY
+        
+        for i in range( 0, n ):
+            s = actions[i].strategy
+            probs[i] = b if s == default_strategy else (1. -b) / (float(n) - 1.)
+
+        return probs
 
 
     ###########################
@@ -42,5 +53,5 @@ class Random_Model(Model):
     def update_model(self, step):
         pass
 
-    def reset(self):
-        pass
+    def reset(self, available_strategies):
+        self.set_available_strategies( available_strategies )
