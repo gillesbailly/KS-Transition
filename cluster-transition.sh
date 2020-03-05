098c33cd428473a3f0bd79176aeb7f2a8bd0654c
gillesbailly <gilles.bailly@sorbonne-universite.fr>
2020-03-05 13:53:54 +0100

    change the python (conda) for the cluster

--- cluster-transition.sh
+++ cluster-transition.sh
@@ -2,5 +2,9 @@
 #PBS -d /home/gbailly/transition_model/KS-Transition
 #PBS -m bae
 #PBS -l nodes=1:ppn=2
-python main-console.py "RW"
-python main-console.py "CK"
+/home/gbailly/miniconda3/bin/python main-console.py "RW" &
+/home/gbailly/miniconda3/bin/python main-console.py "CK" &
+
+wait
+
+echo "C'est fini !"
