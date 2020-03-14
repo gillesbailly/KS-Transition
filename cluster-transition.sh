892d304341044c54e5ea0aa9573abb03a707849c
gillesbailly <gilles.bailly@sorbonne-universite.fr>
2020-03-14 15:03:06 +0100

    for cluster

--- cluster-transition.sh
+++ cluster-transition.sh
@@ -2,11 +2,11 @@
 #PBS -d /home/gbailly/transition_model/KS-Transition
 #PBS -l walltime=24:00:00
 #PBS -m bae
-#PBS -l nodes=1:ppn=2
+#PBS -l nodes=1:ppn=1
 #/home/gbailly/miniconda3/bin/python main-console.py "RW" &
 #/home/gbailly/miniconda3/bin/python main-console.py "CK" &
 #/home/gbailly/miniconda3/bin/python main-console.py "RW_D" &
-/home/gbailly/miniconda3/bin/python main-console.py "RW_CK" &
+#/home/gbailly/miniconda3/bin/python main-console.py "RW_CK" &
 /home/gbailly/miniconda3/bin/python main-console.py "TRANS" &
 
 #/home/gbailly/miniconda3/bin/python main-console.py "RW_CK" &
